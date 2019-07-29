396d169b7193d4bcd7f078186c67645315300857
Andrea Fuentes <andrea.fuentes@peiky.com>
2019-07-28 22:55:32 -0500

    Hello go xD

---
+++ hello.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+// Comentario sencillo xD
+
+func main() {
+	fmt.Println(".....Hello......")
+	time.Sleep(time.Second * 5)
+	fmt.Printf(".....world\n... ... ... ...\n")
+	/*
+		Comentario
+		de
+		varias lineas */
+
+}
