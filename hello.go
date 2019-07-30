53b1acd61de9240e28e5acfa0c0fbeaa3af3ca92
Andrea Fuentes <andrea.fuentes@peiky.com>
2019-07-29 20:36:10 -0500

    struct

--- hello.go
+++ hello.go
@@ -4,9 +4,36 @@ import (
 	"fmt"
 )
 
-// Comentario sencillo xD
+//
+type Cap struct {
+	// Modelos de datos
+	brand string
+	color string
+	price float32
+	curve bool
+}
 
 func main() {
+
+	var cap_black = Cap{
+		brand: "Nike",
+		color: "Black",
+		price: 20.25,
+		curve: true}
+
+	var cap_red = Cap{
+		"Adidas",
+		"Black",
+		24.50,
+		true}
+
+	fmt.Println(".....Black cap")
+	fmt.Println(cap_black.brand)
+	fmt.Println(cap_black.price)
+
+	fmt.Println(".....Red cap")
+
+	fmt.Println(cap_red)
 
 	var num1 float32 = 10
 	var num2 float32 = 6
@@ -14,7 +41,7 @@ func main() {
 	var num3 int = 10
 	var num4 int = 6
 
-	fmt.Print("The sum is: ")
+	fmt.Print("\nThe sum is: ")
 	fmt.Println(num1 + num2)
 
 	fmt.Print("The rest is: ")
